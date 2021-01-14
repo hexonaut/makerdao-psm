cee4e71bc1b003a14f714b23e221d2e0cd418d6e
Aryan Curiel <aryan.curiel@gmail.com>
2021-01-14 15:19:04 +0200

    Showing how to use the service in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { usePsmService } from './services/psm/PsmProvider';
 
 function App() {
   const psmService = usePsmService();
+  const [stats, setStats] = useState(undefined);
+  const [fees, setFees] = useState(undefined);
+  // const [ilk, setIlk] = useState();
+  useEffect(async () => {
+    setStats(await psmService.getStats('USDC'));
+    setFees(await psmService.getFees());
+  }, []);
   return (
     <div>
-      <h1>{psmService.validGems}</h1>
+      <h3>Stats</h3>
+      <ul>
+        {stats && Object.entries(stats).map(([key, value]) => (
+          <li>
+            { key }
+            :
+            { value }
+          </li>
+        ))}
+      </ul>
+      <h3>Fees</h3>
+      {fees && (
+      <ul>
+        <li>
+          TIN:
+          {fees.tin}
+        </li>
+        <li>
+          TOUT:
+          {fees.tout}
+        </li>
+      </ul>
+      )}
     </div>
   );
 }
